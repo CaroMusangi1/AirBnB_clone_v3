cd595da6e00d1ef9abd93c4773149b46005af1a4
CaroMusangi1 <kitongacarol8@gmail.com>
2024-06-08 15:25:56 +0000

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,11 +3,26 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status')
 def api_status():
     """
+Returns na JSON responce for RESTful API health.
 
     """
 responce = {'status': "OK"}
     return jsonify(responce)
+
+@app_views.route('/stats')
+def get_stats():
+    """Endpoint that retrieves the number of each objects by type"""
+    result = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(result)
