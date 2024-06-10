91e11ac09de90cf125767619015a56dda2c081d4
CaroMusangi1 <kitongacarol8@gmail.com>
2024-06-10 14:27:53 +0000

    state

---
+++ api/v1/views/states.py
@@ -0,0 +1,58 @@
+#!/usr/bin/bash
+"""This module handles the HTTP methods for states"""
+
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models.state import State
+from models import storage
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """Returns a list of all states in the database"""
+    states = storage.all(State)
+    return jsonify([state.to_dict() for state in states.values()])
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """Get information about a specific state."""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    """Delete a state from the database given its ID"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """Create a new state with data sent in the request"""
+    data_dict = request.get_json(silent=True)
+    if not data_dict:
+        abort(400, description="Not a JSON")
+    if 'name' not in data_dict:
+        abort(400, description="Missing name")
+    new_state = State(**data_dict)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """Update a state with data sent in the request"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    data_dict = request.get_json(silent=True)
+    if not data_dict:
+        abort(400, description="Not a JSON")
+    for k, v in data_dict.items():
+        if k not in ("id", "created_at", "updated_at"):
+            setattr(state, k, v)
+    state.save()
+    return jsonify(state.to_dict()), 200
